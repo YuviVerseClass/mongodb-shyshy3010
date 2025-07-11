4b232edf302f62f219d30edb8f4fc202c790a571
shyshy3010 <journo.shyrel@gmail.com>
2025-07-11 17:05:29 +0300

    Implement task controller with CRUD operations (get, add, toggle, delete)

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -1,19 +1,46 @@
 const Task = require('../models/Task');
 
 async function getTasks(req, res) {
-  // TODO
+  try {
+    const tasks = await Task.find(); // récupère toutes les tâches
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 async function addTask(req, res) {
-  // TODO
+  try {
+    const newTask = new Task({ title: req.body.title });
+    await newTask.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 }
 
 async function toggleTask(req, res) {
-  // TODO
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).send("Task not found");
+
+    task.done = !task.done; // inverse l'état "done"
+    await task.save();
+    res.json(task);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 async function deleteTask(req, res) {
-  // TODO
+  try {
+    const result = await Task.findByIdAndDelete(req.params.id);
+    if (!result) return res.status(404).send("Task not found");
+
+    res.sendStatus(204); // pas de contenu mais succès
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 }
 
 module.exports = {
